417933e717f54836fbc98fb254026da2bc9df75b
Amir-Pathan <mramir8765@gmail.com>
2022-09-13 13:17:43 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import logo from './logo.svg';
+import { useEffect,useState } from 'react';
 import './App.css';
+import Countdown from './countdown';
+import GoogleLogin from 'react-google-login';
+import { gapi } from 'gapi-script';
+
+var clientid='520218560267-39dp0bdqc3ljaipsra25b783v5hja38t.apps.googleusercontent.com'
 
 function App() {
+
+  const clientId='520218560267-1cscm13162dbnog260uc6mk0hoe16f9v.apps.googleusercontent.com'
+
+  const [loggedIn,setLoggedIn] = useState(false)
+
+  const responseGoogle = (response) => {
+    console.log(response);
+
+    console.log('hello');
+
+    setLoggedIn(true)
+
+  }
+
+
+useEffect(() => {
+  const initClient = () => {
+        gapi.client.init({
+        clientId: clientId,
+        scope: ''
+      });
+   };
+   gapi.load('client:auth2', initClient);
+});
+
+  const logout=()=>{
+  console.log('log out');
+  }
+
   return (
     <div className="App">
-     
+      {
+        loggedIn?
+        <Countdown/>:
+        <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+       // onFailure={}
+        cookiePolicy={'single_host_origin'}
+      />
+
+      }
     </div>
   );
 }
---
+++ src/countdown/countdown.jsx
@@ -0,0 +1,85 @@
+import React,{useState,useEffect} from "react";
+
+function Countdown(){
+
+    const [minute,setMinute] = useState(24)
+
+    const [secound,setSecound] = useState(60)
+
+    const [isCount,setIsCount] = useState(true)
+
+    const [isBreak,setBreak] = useState(false)
+
+    let timeout;
+
+    useEffect(()=>{
+
+    
+    
+        timeout=setTimeout(()=>{
+
+            if(secound===0&&minute===0){
+
+                if(isBreak){
+                    setBreak(false)
+                    setMinute(24)
+                    setSecound(60)
+                }else{
+                    setBreak(true)
+                    setMinute(4)
+                    setSecound(60)
+                }
+
+            }else{
+
+                if(isCount){
+                    if(secound===0){
+                        setSecound(60)
+                        setMinute(minute-1)
+                    }else{
+                        setSecound(secound-1)
+                    }
+                }
+        }
+        },1000)
+        
+
+    },[secound,isCount])
+
+    const countDownHander = (iscount)=>setIsCount(iscount)
+
+    const reset=()=>{
+
+        clearTimeout(timeout)
+
+        if(isBreak){
+
+               
+        setMinute(4)
+        setSecound(60)
+        setIsCount(true)
+
+        }else{
+
+        setMinute(24)
+        setSecound(60)
+        setIsCount(true)
+        }
+
+    }
+
+    return(
+        <>
+          <h5>{isBreak?'break':'count down'}</h5>
+          <h1>{minute}:{secound}</h1>
+          <div>
+            <button onClick={()=>countDownHander(false)}> stop</button>
+            <button onClick={()=>countDownHander(true)}> Pause</button>
+            <button onClick={reset}> Reset</button>
+          </div>
+        </>
+    )
+
+}
+
+export default Countdown
